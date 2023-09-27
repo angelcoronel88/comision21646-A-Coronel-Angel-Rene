ff400aeb9a964ea2d49531ff15c2f47017b2fbc0
Angel Coronel <renecoronel875@gmail.com>
2023-09-26 21:46:27 -0300

    Nuevos cambios

--- README.md
+++ README.md
@@ -5,12 +5,14 @@
 # comision 21646 A -> Coronel Angel
 
 Cada publicacion debe contener:
-* Titulo
-* Contenido
-* Una imagen con su link(url)
-* Fecha en la que fue creada
+*Titulo
+*Contenido
+*Una imagen con su link(url)
+*Fecha en la que fue creada
 
 Para poder ejecutar el servidor:
-** Con el comando "npm run dev"
-** servidor: localhost/3000 
-** Nombre de la base de datos: db_post
+*Con el comando "npm run dev"
+*servidor: localhost/3000 
+*Nombre de la base de datos: db_post
+
+PD: no logre hacer funcionar el " .env "
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,25 +1,24 @@
-const contenedor = document.getElementById('container.row');
-const btnCrear = document.getElementById('btn-new');
-const myModal = new bootstrap.Modal(document.getElementById('myModal'));
-const btnSave = document.getElementById('btn-save');
+const contenedor = document.getElementById("container-row");
+const btnCrear = document.getElementById("btn-new");
+const myModal = new bootstrap.Modal(document.getElementById("myModal"));
+const btnSave = document.getElementById("btn-save");
 const form = document.getElementById("formulario");
 
-let html = '';
-let option = '';
-let idForm = '';
+let html = "";
+let option = "";
+let idForm = "";
 
-const inputTitle = document.getElementById('inputTitle');
-const inputContent = document.getElementById('inputContent');
-const inputUrlLink = document.getElementById('inputUrlLink');
+const inputTitle = document.getElementById("inputTitle");
+const inputContent = document.getElementById("inputContent");
+const inputLinkUrl = document.getElementById("inputUrlLink");
 
-
-btnCrear.addEventListener('click', () => {
-    option = 'new';
-  btnSave.textContent = 'Nuevo';
-  inputTitle.value = '';
-  inputContent.value = '';
-  inputUrlLink.value = '';
-  myModal.show()
+btnCrear.addEventListener("click", () => {
+  option = "new";
+  btnSave.textContent = "Nuevo";
+  inputTitle.value = "";
+  inputContent.value = "";
+  inputUrlLink.value = "";
+  myModal.show();
 });
 
 
@@ -28,104 +27,86 @@ document.addEventListener('click', (event) => {
         const article = event.target.closest('.col-4')
         const idArticle = article.dataset.id
 
-        Swal.fire({
-            title: 'Estás seguro?',
-            text: "No podrás revertir esto!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: 'black',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Si, borralo!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                fetch(`http://localhost:3000/api/posts/${idArticle}`, {
-                    method: 'DELETE'
-                }).then(res => {
-                    if (res.ok) {
-                        article.remove()
-                    }
-                }).catch(err => {
-                     console.error(err)
-                })
-                Swal.fire(
-                    'Borrado!',
-                    'Su posteo fue borrado.',
-                    'Exito'
-                )
+        fetch(`http://localhost:3000/api/posts/${idArticle}`, {
+            method: 'DELETE'
+        }).then(res => {
+            if (res.ok) {
+                article.remove()
             }
+        }).catch(err => {
+            console.error(err)
         })
     }
+})
+
+document.addEventListener("click", (event) => {
+  if (event.target.matches("#btn-edit")) {
+    const article = event.target.closest(".col-4");
+
+    const idArticle = article.dataset.id;
+    const linkUrlEdit = article.children[0].children[0].src;
+    const titleEdit = article.children[0].children[1].children[0].textContent;
+    const contentEdit = article.children[0].children[1].children[1].textContent;
+
+    idForm = idArticle;
+    inputTitle.value = titleEdit;
+    inputContent.value = contentEdit;
+    inputUrlLink.value = linkUrlEdit;
+    option = "edit";
+    btnSave.textContent = "Editar";
+    myModal.show();
+  }
 });
-
-document.addEventListener('click', (event) => {
-    if (event.target.matches("#btn-edit")) {
-      const article = event.target.closest(".col-4");
-      
-      const idArticle = article.database.id;
-      const urlLinkEdit = article.children[0].children[0].src;
-      const titleEdit = article.children[0].children[1].children[0].textContent;
-      const contentEdit = article.children[0].children[1].children[1].textContent;
-
-        idForm = idArticle;
-        inputTitle.value = titleEdit;
-        inputContent.value = contentEdit;
-        inputUrlLink.value = urlLinkEdit;
-        option = "edit";
-        btnSave.textContent = "Editar";
-        myModal.show();
-    }
-});
-
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    // console.log("submit");
+  event.preventDefault();
+  // console.log("submit");
 
-    if (option === "new") {
-        const newPost = {
-          title: inputTitle.value,
-          content: inputContent.value,
-          urlLink: inputUrlLink.value,
-        };
-    
-        fetch('http://localhost:3000/api/posts', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newPost)
-        }).then(res => {
-            console.log(res)
-            if (res.ok) {
-                alert("Post creado con exito!");
-                myModal.hide();
-                location.reload();
-            }
-        })
-        .catch((err) => {
-            console.error(err);
-       });
-    }
+  if (option === "new") {
+    const newPost = {
+      title: inputTitle.value,
+      content: inputContent.value,
+      urlLink: inputUrlLink.value,
+    };
 
-    if (option === "edit") {
-        const newPost = {
-          title: inputTitle.value,
-          content: inputContent.value,
-          urlLink: inputUrlLink.value,
-        };
-        
-        fetch (`http://localhost:3000/api/posts/${idForm}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newPost)
-        }).then(res => {
-            if(res.ok){
-                alert('El posteo fue editado exitosamente')
-                myModal.hide();
-                location.reload();
-            }
-        })
-    }
+    fetch('http://localhost:3000/api/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newPost)
+    }).then(res => {
+      console.log(res)
+        if (res.ok) {
+          alert("Posteo creado exitosamente");
+          myModal.hide();
+          location.reload();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  if (option === "edit") {
+    const newPost = {
+      title: inputTitle.value,
+      content: inputContent.value,
+      urlLink: inputUrlLink.value,
+    };
+
+    fetch(`http://localhost:3000/api/posts/${idForm}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newPost)
+    }).then(res => {
+      if(res.ok){
+        alert('Posteo editado exitosamente')
+        myModal.hide();
+        location.reload();
+      }
+    })
+  }
 });
